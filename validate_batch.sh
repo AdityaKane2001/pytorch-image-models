2955f2d45c5267d9a9986504fc73fe5addf18b50
AdityaKane2001 <adityakane1@gmail.com>
2024-11-11 09:01:39 -0800

    sync

--- validate_batch.sh
+++ validate_batch.sh
@@ -2,15 +2,23 @@ readarray -t modellist < /workspace/akane/node-modellist.txt
 
 for MODEL in "${modellist[@]}"
 do
+    echo "#############################################################"
+    echo "Running $MODEL unpruned..."
     ./unpruned.sh $MODEL 2>&1 | tee -a outfiles/$MODEL-unpruned.txt
+    echo "=========================="
 
     for num in $(seq 1 16)
     do
+        echo "=========================="
+        echo "Running $MODEL arithmetic mean with num=$num..."
         ./arithmetic_mean.sh $MODEL $num 2>&1 | tee -a outfiles/$MODEL-arithmetic-mean-$num.txt
+        echo "=========================="
+        echo "Running $MODEL geometric mean with num=$num..."
         ./geometric_mean.sh $MODEL $num 2>&1 | tee -a outfiles/$MODEL-geometric-mean-$num.txt
 
         for k in $(seq 1 5)
         do
+            echo "Running $MODEL topk with num=$num and k=$k..."
             ./topk.sh $MODEL $num $k 2>&1 | tee -a outfiles/$MODEL-topk-$num-$k.txt
         done
     done
