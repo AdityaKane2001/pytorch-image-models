dc40ef154cbf1fbd24288219f7b032396f047da5
AdityaKane2001 <adityakane1@gmail.com>
2024-12-02 22:42:10 -0800

    change path

--- geometric_mean.sh
+++ geometric_mean.sh
@@ -1,16 +1,18 @@
 python3 -u validate.py --data-dir /data/datasets/ImageNet/ \
-   --model=$1\
-   --evict-algo="geometric_mean"\
-   --evict-k=1000\
-   --evict-start=0.5\
-   --evict-end=-1\
-   --evict-num=$2\
-   --evict-after-end=-1\
-   --amp\
-   --results-format="json"\
-   --batch-size=1024\
-   --workers=32\
-   --num-gpu=8\
-   --savedir="/home/users/akane/kv-evict-results/$1/geometric_mean/"\
-   --pretrained
+    --model=$1\
+    --evict-algo="geometric_mean"\
+    --evict-policy="0.25,0.5,0.75"\
+    --evict-k=1000\
+    --evict-start=0.5\
+    --evict-end=-1\
+    --evict-num=$2\
+    --evict-after-end=-1\
+    --amp\
+    --results-format="json"\
+    --batch-size=1024\
+    --workers=32\
+    --num-gpu=8\
+    --num-gemms=2\
+    --savedir="/home/users/akane/kv-evict-results-3spot-policy/$1/geometric_mean/"\
+    --pretrained
 
--- topk.sh
+++ topk.sh
@@ -1,16 +1,17 @@
 python3 -u validate.py --data-dir /data/datasets/ImageNet/ \
-   --model=$1\
-   --evict-algo="topk"\
-   --evict-k=$3\
-   --evict-start=0.5\
-   --evict-end=-1\
-   --evict-num=$2\
-   --evict-after-end=-1\
-   --amp\
-   --results-format="json"\
-   --batch-size=1024\
-   --workers=32\
-   --num-gpu=8\
-   --savedir="/home/users/akane/kv-evict-results/$1/topk/"\
-   --pretrained
+    --model=$1\
+    --evict-algo="topk"\
+    --evict-policy="0.25,0.5,0.75"\
+    --evict-k=$3\
+    --evict-start=-1\
+    --evict-end=-1\
+    --evict-num=$2\
+    --evict-after-end=-1\
+    --amp\
+    --results-format="json"\
+    --batch-size=1024\
+    --workers=32\
+    --num-gpu=8\
+    --savedir="/home/users/akane/kv-evict-results-3spot-policy/$1/topk/"\
+    --pretrained
 
